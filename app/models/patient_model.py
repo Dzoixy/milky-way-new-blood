8ca7fbce6045917cff59af9debeecb98323730b3
Dzoixy <sittichaisc1@gmail.com>
2026-02-20 02:27:41 +0700

    fix patient_model again.py

--- app/models/patient_model.py
+++ app/models/patient_model.py
@@ -1,14 +1,11 @@
+from datetime import date
 from sqlalchemy import String, Integer, Date
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database.connection import Base
+id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)  
+full_name: Mapped[str] = mapped_column(String)  
+national_id: Mapped[str] = mapped_column(String, unique=True)  
+date_of_birth: Mapped[date] = mapped_column(Date)  
+gender: Mapped[str] = mapped_column(String)  
 
-class Patient(Base):
-    __tablename__ = "patients"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    full_name: Mapped[str] = mapped_column(String)
-    national_id: Mapped[str] = mapped_column(String, unique=True)
-    date_of_birth: Mapped[Date]
-    gender: Mapped[str] = mapped_column(String)
-
-    visits = relationship("Visit", back_populates="patient")
+visits = relationship("Visit", back_populates="patient")
