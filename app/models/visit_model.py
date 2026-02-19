7f3ea7a12a4075726fbfd951aadb016e4171ac76
Dzoixy <sittichaisc1@gmail.com>
2026-02-20 02:37:20 +0700

    fix visit_model.py.py

--- app/models/visit_model.py
+++ app/models/visit_model.py
@@ -1,18 +1,23 @@
+from datetime import datetime
 from sqlalchemy import Integer, Float, ForeignKey, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from datetime import datetime
-from database.connection import Base
+from app.database.connection import Base
+
 
 class Visit(Base):
     __tablename__ = "visits"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    patient_id: Mapped[int] = mapped_column(ForeignKey("patients.id"))
+    patient_id: Mapped[int] = mapped_column(ForeignKey("patients.id"), nullable=False)
 
-    sbp: Mapped[float]
-    dbp: Mapped[float]
-    risk_score: Mapped[float]
+    sbp: Mapped[float] = mapped_column(Float, nullable=False)
+    dbp: Mapped[float] = mapped_column(Float, nullable=False)
+    risk_score: Mapped[float] = mapped_column(Float, nullable=False)
 
-    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow,
+        nullable=False
+    )
 
     patient = relationship("Patient", back_populates="visits")
