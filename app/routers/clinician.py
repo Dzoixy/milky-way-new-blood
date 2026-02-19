81b68f61c87d8c4c3a32c9bfeff171aaaa0ac801
Dzoixy <sittichaisc1@gmail.com>
2026-02-20 03:33:01 +0700

    fix clinician.py,html

--- app/routers/clinician.py
+++ app/routers/clinician.py
@@ -1,13 +1,21 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from datetime import datetime
 
 router = APIRouter(prefix="/clinician")
 templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/dashboard", response_class=HTMLResponse)
 async def clinician_dashboard(request: Request):
+
+    patients = []   # ตอนนี้ยังไม่มีข้อมูลผู้ใช้
+
     return templates.TemplateResponse(
         "dashboard_clinician.html",
-        {"request": request}
+        {
+            "request": request,
+            "patients": patients,
+            "current_date": datetime.now().strftime("%A, %d %B %Y")
+        }
     )
