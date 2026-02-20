ce15f68ef2c2de11e09757288f01fddb413c44af
Dzoixy <sittichaisc1@gmail.com>
2026-02-21 03:10:21 +0700

    fix main.py

--- app/main.py
+++ app/main.py
@@ -2,27 +2,41 @@ import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.middleware.sessions import SessionMiddleware
 
 from app.routers import auth, clinician, patient, risk
 from app.database.connection import engine, Base
 
-# สร้าง app ก่อนเสมอ
 app = FastAPI(title="Milky Way")
 
-# Static files
+# -------------------------
+# -------------------------
+app.add_middleware(
+    SessionMiddleware,
+    secret_key="super-secret-key-change-this"
+)
+
+# -------------------------
+# Static
+# -------------------------
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
+# -------------------------
 # Templates
+# -------------------------
 templates = Jinja2Templates(directory="app/templates")
 
-# Include routers
+# -------------------------
+# Routers
+# -------------------------
 app.include_router(auth.router)
 app.include_router(clinician.router)
 app.include_router(patient.router)
 app.include_router(risk.router)
 
-
-# Startup event (สร้าง table แบบ async)
+# -------------------------
+# Startup
+# -------------------------
 @app.on_event("startup")
 async def startup():
     async with engine.begin() as conn:
