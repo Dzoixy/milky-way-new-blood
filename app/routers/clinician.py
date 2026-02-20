ecc53d64102402f8e54bf125cc39f0683d37bcce
Dzoixy <sittichaisc1@gmail.com>
2026-02-21 03:27:35 +0700

    fix clinician.py

--- app/routers/clinician.py
+++ app/routers/clinician.py
@@ -1,3 +1,11 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter(prefix="/clinician", tags=["Clinician"])
+templates = Jinja2Templates(directory="app/templates")
+
+
 @router.get("/dashboard")
 async def clinician_dashboard(request: Request):
 
