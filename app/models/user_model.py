307f069ead37637126fda8871911106956342fc0
Dzoixy <sittichaisc1@gmail.com>
2026-02-21 16:34:44 +0700

    fi user_module.py

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,11 +1,30 @@
 from sqlalchemy import String, Integer
 from sqlalchemy.orm import Mapped, mapped_column
-from database.connection import Base
+from app.database.connection import Base
+
 
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(String, unique=True)
-    password: Mapped[str] = mapped_column(String)
-    role: Mapped[str] = mapped_column(String)
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    username: Mapped[str] = mapped_column(
+        String(100),
+        unique=True,
+        index=True,
+        nullable=False
+    )
+
+    password_hash: Mapped[str] = mapped_column(
+        String(255),   # รองรับ bcrypt hash
+        nullable=False
+    )
+
+    role: Mapped[str] = mapped_column(
+        String(20),
+        nullable=False
+    )
